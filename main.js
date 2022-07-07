b589dca6fdd8f8787cead5f0e15dc6f15cf2daf6
Adhambek0202 <adhambekismatov0212@gmail.com>
2022-07-07 11:51:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var borish = 500;
+var mehmonxona = 250;
+var muzey = 120;
+
+var userNames = prompt("iltimos ismingizni kiriting")
+
+var xarajat = prompt("pulingizni kiriting..!")
+var umumiy = (borish+mehmonxona) * 11000 + muzey*11800;
+
+
+if(xarajat > umumiy) {
+    alert("oq yol Alisher")
+    console.log("oq yol Alisher")
+}else{
+    alert("Alisher ozgina sabr qilish kerak ekan")
+    console.log("Alisher ozgina sabr qilish kerak ekan")
+}
